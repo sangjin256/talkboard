550d600d3bf2f3a822c449e1c0de949edc353084
Park Jinhyeok <harunohi0808@gmail.com>
2025-06-19 18:48:43 +0900

    Update | UI_Board.cs

--- Assets/02. Scripts/Post/4.UI/UI_Board.cs
+++ Assets/02. Scripts/Post/4.UI/UI_Board.cs
@@ -11,5 +11,59 @@ public class UI_Board : MonoBehaviour
     private void Awake()
     {
         _contentPreviewList = new List<UI_ContentPreview>();
+    } 
+    
+    /*
+private void OnEnable()
+{
+    RefreshMyRankingSlot();
+    RefreshRankingList();
+}
+
+public void RefreshMyRankingSlot()
+{
+    int myRanking = RankingManager.Instance.GetPlayerRankNumber();
+    RankingDTO myRankingData = RankingManager.Instance.GetPlayerRankData();
+    _myRankingSlot.RefreshRankingSlot(myRanking, myRankingData.Nickname, myRankingData.KillCount);
+}
+
+public void RefreshRankingList()
+{
+    List<RankingDTO> rankingList = RankingManager.Instance.GetSortedRankList(50);
+
+    SetSlotSize(rankingList.Count);
+
+    for (int i = 0; i < rankingList.Count; ++i)
+    {
+        _rankingSlotList[i].RefreshRankingSlot(
+            i + 1,
+            rankingList[i].Nickname,
+            rankingList[i].KillCount
+        );
+        _rankingSlotList[i].transform.SetSiblingIndex(i);
+        _rankingSlotList[i].gameObject.SetActive(true);
     }
+}
+
+private void SetSlotSize(int size)
+{
+    if (_rankingSlotList.Count < size)
+    {
+        for (int i = _rankingSlotList.Count; i < size; ++i)
+        {
+            UI_RankingSlot newSlot = Instantiate(_rankingSlotPrefab, transform);
+            newSlot.transform.SetParent(Content.transform, false);
+            _rankingSlotList.Add(newSlot);
+        }
+    }
+    else if (_rankingSlotList.Count > size)
+    {
+        for (int i = _rankingSlotList.Count - 1; i >= size; --i)
+        {
+            _rankingSlotList[i].gameObject.SetActive(false);
+        }
+    }
+}
+*/
+    
 }
