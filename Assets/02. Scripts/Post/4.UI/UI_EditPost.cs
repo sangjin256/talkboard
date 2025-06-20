f92d62412ca63caa54a5c6f7195f9e84474fb58c
Park Jinhyeok <harunohi0808@gmail.com>
2025-06-20 12:53:44 +0900

    Feat | EditPanel

---
+++ Assets/02. Scripts/Post/4.UI/UI_EditPost.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using TMPro;
+
+public class UI_EditPost : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField _inputField;
+
+    public async void OnClickEditPostButton()
+    {
+        string content = _inputField.text;
+        PostDTO currentPost = UI_Manager.Instance.Post;
+
+        if (string.IsNullOrEmpty(content))
+        {
+            UI_Manager.Instance.SetNotification("내용을 입력해주세요.");
+            return;
+        }
+
+        Result result = await PostManager.Instance.TryEditPost(currentPost, content);
+        
+        if (result.IsSuccess)
+        {
+            UI_Manager.Instance.SetNotification("게시글이 수정되었습니다.");
+            _inputField.text = string.Empty;
+            gameObject.SetActive(false);
+            await UI_Manager.Instance.Board.UpdatePostPreviewList();
+        }
+        else
+        {
+            UI_Manager.Instance.SetNotification(result.Message);
+        }
+    }
+    
+    public void OnClickCancelButton()
+    {
+        _inputField.text = "";
+        gameObject.SetActive(false);
+    }
+}
